281a18c607236ee12f92a20cd9483f0f4a0e88ff
medhani premathilaka <mpremathilaka1@gmail.com>
2024-11-28 09:06:22 +0530

    added new set of data

--- slearning/lib/screens/register.dart
+++ slearning/lib/screens/register.dart
@@ -220,7 +220,7 @@ class _RegisterState extends State<Register> {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
           content: Text("All fields are required."),
-          backgroundColor: Colors.red,
+          backgroundColor:  Color.fromARGB(159, 10, 174, 49),,
         ),
       );
       return;
@@ -234,7 +234,7 @@ class _RegisterState extends State<Register> {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
             content: Text("Registration Successful! Welcome, $email."),
-            backgroundColor: Colors.green,
+            backgroundColor:  Color.fromRGBO(00, 00, 00, 0.0),
           ),
         );
 
@@ -248,7 +248,7 @@ class _RegisterState extends State<Register> {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: Text("Error: ${e.toString()}"),
-          backgroundColor: Colors.red,
+          backgroundColor:  Color.fromARGB(159, 10, 174, 49),,
         ),
       );
     }
